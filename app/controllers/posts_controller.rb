111ef96970facf3b2eb4edbce74975a1ec882f54
Thiago Pereira <tp.maiochi@gmail.com>
2019-09-23 20:33:29 -0300

    #1: manutencao de posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,42 @@
 class PostsController < ApplicationController
-    def show
-        @post = Post.find(params[:id])
-    end
-    
-    def new
-        @post = Post.new
-    end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-    def create
-        @post = Post.new(post_params)
+  def new
+    @post = Post.new
+  end
 
-        @post.save
-        redirect_to @post
-    end
+  def create
+    @post = Post.new(post_params)
 
-    private
-      def post_params
-        params.require(:post).permit(:title, :body)
-      end
+    @post.save
+    redirect_to @post
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
