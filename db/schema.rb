272b9b5a117469ff990fbf57a70958dec1ff1352
Thiago Pereira <tp.maiochi@gmail.com>
2019-10-28 20:35:37 -0300

    #1: manutencao de posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_224736) do
+ActiveRecord::Schema.define(version: 2019_10_28_225717) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
